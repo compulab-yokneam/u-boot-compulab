cf71fb4e811d67b15514a7b84b61587c207f6778
Valentin Raevsky <valentin@compulab.co.il>
2020-07-17 21:17:19 +0300

    bi_dram: Skip zero size banks
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>
    
    %% original patch: 0003-bi_dram-Skip-zero-size-banks.patch

--- arch/arm/lib/bootm.c
+++ arch/arm/lib/bootm.c
@@ -143,6 +143,8 @@ static void setup_memory_tags(bd_t *bd)
 	int i;
 
 	for (i = 0; i < CONFIG_NR_DRAM_BANKS; i++) {
+		if (!gd->bd->bi_dram[i].size)
+			continue;
 		params->hdr.tag = ATAG_MEM;
 		params->hdr.size = tag_size (tag_mem32);
 
--- arch/arm/lib/cache-cp15.c
+++ arch/arm/lib/cache-cp15.c
@@ -96,6 +96,8 @@ __weak void dram_bank_mmu_setup(int bank)
 	     i < (bd->bi_dram[bank].start >> MMU_SECTION_SHIFT) +
 		 (bd->bi_dram[bank].size >> MMU_SECTION_SHIFT);
 	     i++) {
+		if(!bd->bi_dram[bank].size)
+			continue;
 #if defined(CONFIG_SYS_ARM_CACHE_WRITETHROUGH)
 		set_section_dcache(i, DCACHE_WRITETHROUGH);
 #elif defined(CONFIG_SYS_ARM_CACHE_WRITEALLOC)
