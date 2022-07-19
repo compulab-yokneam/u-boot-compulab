1b56d53a391b2b9b69470857d91d0d220fdfe247
Valentin Raevsky <valentin@compulab.co.il>
2022-07-19 16:28:36 +0300

    iot-gate-imx8plus: eeprom: Read mac from the dev_id offest
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>
    
    %% original patch: 0060-iot-gate-imx8plus-eeprom-Read-mac-from-the-dev_id-of.patch

--- board/compulab/iot-gate-imx8plus/iot-gate-imx8plus.c
+++ board/compulab/iot-gate-imx8plus/iot-gate-imx8plus.c
@@ -33,7 +33,7 @@ DECLARE_GLOBAL_DATA_PTR;
 #include "../common/eeprom.h"
 void imx_get_mac_from_fuse(int dev_id, unsigned char *mac)
 {
-	cl_eeprom_read_n_mac_addr(mac, /*dev_id*/ 0, CONFIG_SYS_I2C_EEPROM_BUS);
+	cl_eeprom_read_n_mac_addr(mac, dev_id, CONFIG_SYS_I2C_EEPROM_BUS);
 	debug("%s: MAC%d: %02x.%02x.%02x.%02x.%02x.%02x\n",
 	      __func__, dev_id, mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 	return;
