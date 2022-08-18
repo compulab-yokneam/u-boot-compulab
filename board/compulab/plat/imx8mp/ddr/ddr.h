f50567d5512f84d983c697082cee02ad123fa325
Valentin Raevsky <valentin@compulab.co.il>
2022-08-18 09:36:07 +0300

    imx8mp: ddr: Add Nanya 2G support
    Add Nanya [mr5-mr8]=0x05000010 support
    
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>
    
    %% original patch: 0070-imx8mp-ddr-Add-Nanya-2G-support.patch

--- board/compulab/plat/imx8mp/ddr/ddr.h
+++ board/compulab/plat/imx8mp/ddr/ddr.h
@@ -46,6 +46,11 @@ static const struct lpddr4_desc lpddr4_array[] = {
 		.timing = &ucm_dram_timing_01061010_2G
 #endif
 	},
+	{ .name = "Nanya",	.id = 0x05000010, .subind = 0xff, .size = 2048, .count = 1,
+#ifdef CONFIG_SPL_BUILD
+		.timing = &ucm_dram_timing_01061010_2G
+#endif
+	},
 	{ .name = "Samsung",	.id = 0x01061010, .subind = 0xff, .size = 2048, .count = 1,
 #ifdef CONFIG_SPL_BUILD
 		.timing = &ucm_dram_timing_01061010_2G
