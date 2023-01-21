87bac9b4e340737e1972447926ed3bb81a6669be
Valentin Raevsky <valentin@compulab.co.il>
2023-01-21 21:25:15 +0200

    compulab: imx8mp: Add efi_capsule.c
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

---
+++ board/compulab/plat/imx8mp/board/efi_capsule.c
@@ -0,0 +1,20 @@
+#include <common.h>
+#include <efi_loader.h>
+
+#if CONFIG_IS_ENABLED(EFI_HAVE_CAPSULE_SUPPORT)
+struct efi_fw_image fw_images[] = {
+	{
+		.image_type_id = IMX_BOOT_IMAGE_GUID,
+		.fw_name = u"CLAB-IMX8MP-RAW",
+		.image_index = 1,
+	},
+};
+
+struct efi_capsule_update_info update_info = {
+	.dfu_string = "mmc 2=flash-bin raw 0 0x2000 mmcpart 1",
+	.images = fw_images,
+};
+
+u8 num_image_type_guids = ARRAY_SIZE(fw_images);
+#endif /* EFI_HAVE_CAPSULE_SUPPORT */
+
