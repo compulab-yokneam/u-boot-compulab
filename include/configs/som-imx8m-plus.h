3eb204ae30f143f951aa745d6589b4c200e8e89f
Valentin Raevsky <valentin@compulab.co.il>
2023-02-27 18:43:48 +0200

    som-imx8m-plus: Enable FEC_QUIRK_ENET_MAC
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- include/configs/som-imx8m-plus.h
+++ include/configs/som-imx8m-plus.h
@@ -16,6 +16,7 @@
 
 #define CONFIG_FEC_XCV_TYPE             RGMII
 #define CONFIG_FEC_MXC_PHYADDR         	-1
+#define FEC_QUIRK_ENET_MAC
 
 #define DWC_NET_PHYADDR	               	-1
 #define PHY_ANEG_TIMEOUT 20000
