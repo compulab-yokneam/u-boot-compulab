97c9e6aebc67a05cd99cfd40c6c38d18f0cc0292
Valentin Raevsky <valentin@compulab.co.il>
2023-02-27 18:43:06 +0200

    iot-gate-imx8plus: Enable phy address discovery logics
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- include/configs/iot-gate-imx8plus.h
+++ include/configs/iot-gate-imx8plus.h
@@ -15,10 +15,12 @@
 #define CONFIG_ETHPRIME                 "eth0" /* Set eqos to primary since we use its MDIO */
 
 #define CONFIG_FEC_XCV_TYPE             RGMII
+#define CONFIG_FEC_MXC_PHYADDR         	-1
 #define FEC_QUIRK_ENET_MAC
 
 #ifdef CONFIG_DWC_ETH_QOS
 #define CONFIG_SYS_NONCACHED_MEMORY     (1 * SZ_1M)     /* 1M */
+#define DWC_NET_PHYADDR	               	-1
 #endif
 
 #define PHY_ANEG_TIMEOUT 20000
