97c9b34c64ddb2cf0308e8b36fdd1b8effa9440b
Valentin Raevsky <valentin@compulab.co.il>
2023-12-05 07:50:49 +0200

    LF-9055 imx8m: Fix iMX8MP QuadLite kernel boot hang
    Original commit: a1293b1fa23f552f81e8c1b6b593ff703f60e6b8
    
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- arch/arm/mach-imx/imx8m/soc.c
+++ arch/arm/mach-imx/imx8m/soc.c
@@ -820,13 +820,19 @@ int disable_vpu_nodes(void *blob)
 	static const char * const nodes_path_8mm[] = {
 		"/vpu_g1@38300000",
 		"/vpu_g2@38310000",
-		"/vpu_h1@38320000"
+		"/vpu_h1@38320000",
+		"/soc@0/blk-ctrl@38330000"
 	};
 
 	static const char * const nodes_path_8mp[] = {
 		"/vpu_g1@38300000",
 		"/vpu_g2@38310000",
-		"/vpu_vc8000e@38320000"
+		"/vpu_vc8000e@38320000",
+		"/soc@0/blk-ctl@38330000",
+		"/soc@0/bus@30000000/gpc@303a0000/pgc/power-domain@19",
+		"/soc@0/bus@30000000/gpc@303a0000/pgc/power-domain@20",
+		"/soc@0/bus@30000000/gpc@303a0000/pgc/power-domain@21",
+		"/soc@0/bus@30000000/gpc@303a0000/pgc/power-domain@22"
 	};
 
 	if (is_imx8mq())
