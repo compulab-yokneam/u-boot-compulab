461c5c02eb7d867d8ba1abdf9539814df3f81187
Valentin Raevsky <valentin@compulab.co.il>
2024-02-04 18:51:41 +0200

    compulab: mcm-imx8m-plus: Add the machine support
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- include/configs/mcm-imx8m-plus.h
+++ include/configs/mcm-imx8m-plus.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 CompuLab
+ * Copyright 2024 CompuLab
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
@@ -9,17 +9,11 @@
 
 #include "compulab-imx8m-plus.h"
 
-#define CONFIG_IMX6_PWM_PER_CLK 66000000
-
 #if defined(CONFIG_CMD_NET)
-#define CONFIG_ETHPRIME                 "eth1" /* Set eqos to primary since we use its MDIO */
-
-#define CONFIG_FEC_XCV_TYPE             RGMII
-#define CONFIG_FEC_MXC_PHYADDR         	-1
+#define CFG_FEC_MXC_PHYADDR         	-1
 #define FEC_QUIRK_ENET_MAC
 
 #ifdef CONFIG_DWC_ETH_QOS
-#define CONFIG_SYS_NONCACHED_MEMORY     (1 * SZ_1M)     /* 1M */
 #define DWC_NET_PHYADDR	               	-1
 #endif
 
