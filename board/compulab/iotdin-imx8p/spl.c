142473e2c2256f30874ae480b0621a9c47e9f056
Valentin Raevsky <valentin@compulab.co.il>
2024-05-13 11:22:10 +0300

    compulab: Add iotd-imx8p support
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

---
+++ board/compulab/iotdin-imx8p/spl.c
@@ -0,0 +1,9 @@
+#include <asm/io.h>
+#include <asm/mach-imx/iomux-v3.h>
+#include <asm/arch/imx8mp_pins.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/sys_proto.h>
+
+int spl_board_private_init(void) {
+	return 0;
+}
