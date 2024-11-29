fdfdfa791376ce8a9cc00725503809063e49138a
Valentin Raevsky <valentin@compulab.co.il>
2024-11-29 17:40:59 +0200

    compulab: im8mp: ddr: Expand Samsung support with 0x01080010
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- board/compulab/plat/imx8mp/ddr/ddr.h
+++ board/compulab/plat/imx8mp/ddr/ddr.h
@@ -77,6 +77,11 @@ static const struct lpddr4_desc lpddr4_array[] = {
 		.timing = &ucm_dram_timing_01061010_2G
 #endif
 	},
+	{ .name = "Samsung",	.id = 0x01080010, .subind = 0x2, .size = 2048, .count = 1,
+#ifdef CONFIG_SPL_BUILD
+		.timing = &ucm_dram_timing_01061010_2G
+#endif
+	},
 #else
 	{ .name = "deadbeaf",	.id = 0xdeadbeaf, .subind = 0x1, .size = 1024, .count = 1,
 #ifdef CONFIG_SPL_BUILD
