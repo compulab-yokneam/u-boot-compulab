c3a8bca97ad1ae7844768642985607fdd5b44187
Valentin Raevsky <valentin@compulab.co.il>
2025-02-10 17:44:18 +0200

    Revert "mcm-imx93: add gpio system reset"
    This reverts commit e2b62304cf4c3ff40079ab38fa29e1d149c6151d.

--- board/compulab/common-imx9/reset-mcm-imx9.c
+++
@@ -1,9 +0,0 @@
-#include <common.h>
-#include <asm/io.h>
-
-#define PIN9 0x200
-#define GPIO1_PORT_DATA_DIRECTION_REG 0x47400054
-
-void reset_board(void) {
-	writel(PIN9, GPIO1_PORT_DATA_DIRECTION_REG);
-}
