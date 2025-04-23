c7024793d866fd8f2a2f812138213054e6098fb2
Valentin Raevsky <valentin@compulab.co.il>
2025-04-23 16:33:39 +0300

    ucm-imx91: Redefine imx_get_mac_from_fuse()
    Redefine imx_get_mac_from_fuse().
    Make the function read mac from the SOM eeprom instead of the soc fuses.
    
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- board/compulab/ucm-imx91/ucm-imx91.c
+++ board/compulab/ucm-imx91/ucm-imx91.c
@@ -98,6 +98,16 @@ int board_phy_config(struct phy_device *phydev)
 	return 0;
 }
 
+#if defined(CONFIG_FEC_MXC) || defined(CONFIG_DWC_ETH_QOS)
+void imx_get_mac_from_fuse(int dev_id, unsigned char *mac)
+{
+	cl_eeprom_read_n_mac_addr(mac, dev_id, CONFIG_SYS_I2C_EEPROM_BUS);
+	debug("%s: MAC%d: %02x.%02x.%02x.%02x.%02x.%02x\n",
+	      __func__, dev_id, mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+	return;
+}
+#endif
+
 struct gpio_desc ext_pwren_desc, exp_sel_desc;
 
 static void board_gpio_init(void)
