ff3269365e237be71a2324efd2872cd76614b243
Ilya Ledvich <ilya@compulab.co.il>
2025-06-25 14:59:01 +0300

    iot-gate-imx8plus: add-on board detection: add support for M2M2 board
    Add support for M2 add-on board detection. These add-on boards can be connected
    via M.2 Expansion Connector and feature:
     - M.2 key E connector to connect IoT Connectivity Cards by CTHINGS.CO over USB
     - IMU: ICM-42605 over I2C
     - TPM: SLB9670 over SPI
    
    Signed-off-by: Ilya Ledvich <ilya@compulab.co.il>

--- board/compulab/iot-gate-imx8plus/iot-gate-imx8plus.c
+++ board/compulab/iot-gate-imx8plus/iot-gate-imx8plus.c
@@ -33,6 +33,7 @@ DECLARE_GLOBAL_DATA_PTR;
 typedef enum {
 	IOTG_IMX8PLUS_ADDON_FIRST,
 	IOTG_IMX8PLUS_ADDON_M2EMMC = IOTG_IMX8PLUS_ADDON_FIRST, /* eMMC+TPM module */
+	IOTG_IMX8PLUS_ADDON_M2M2, /* M.2 slot+IMC+TPM module */
 	IOTG_IMX8PLUS_ADDON_M2ADC, /* ADC+TPM module */
 	IOTG_IMX8PLUS_ADDON_M2TPM, /* TPM module */
 	IOTG_IMX8PLUS_ADDON_EMPTY,
@@ -42,6 +43,7 @@ typedef enum {
 
 static char *iotg_imx8plus_addon_type_name[IOTG_IMX8PLUS_ADDON_NUM] = {
 	[IOTG_IMX8PLUS_ADDON_M2EMMC] = "M2EMMC",
+	[IOTG_IMX8PLUS_ADDON_M2M2] = "M2M2",
 	[IOTG_IMX8PLUS_ADDON_M2ADC] = "M2ADC",
 	[IOTG_IMX8PLUS_ADDON_M2TPM] = "M2TPM",
 	[IOTG_IMX8PLUS_ADDON_EMPTY] = "none",
@@ -50,6 +52,7 @@ static char *iotg_imx8plus_addon_type_name[IOTG_IMX8PLUS_ADDON_NUM] = {
 /* Device tree names array */
 static char *iotg_imx8plus_dtb[IOTG_IMX8PLUS_ADDON_NUM] = {
 	[IOTG_IMX8PLUS_ADDON_M2EMMC] = "iot-gate-imx8plus-m2emmc.dtb",
+	[IOTG_IMX8PLUS_ADDON_M2M2] = "iot-gate-imx8plus-m2m2.dtb",
 	[IOTG_IMX8PLUS_ADDON_M2ADC] = "iot-gate-imx8plus-m2adc.dtb",
 	[IOTG_IMX8PLUS_ADDON_M2TPM] = "iot-gate-imx8plus-m2tpm.dtb",
 	[IOTG_IMX8PLUS_ADDON_EMPTY] = "iot-gate-imx8plus.dtb",
@@ -58,6 +61,7 @@ static char *iotg_imx8plus_dtb[IOTG_IMX8PLUS_ADDON_NUM] = {
 /* I2C bus numbers array */
 static int iotg_imx8plus_addon_i2c_bus[IOTG_IMX8PLUS_ADDON_LAST] = {
 	[IOTG_IMX8PLUS_ADDON_M2EMMC] = 4,
+	[IOTG_IMX8PLUS_ADDON_M2M2] = 4,
 	[IOTG_IMX8PLUS_ADDON_M2ADC] = 4,
 	[IOTG_IMX8PLUS_ADDON_M2TPM] = 4,
 };
@@ -65,6 +69,7 @@ static int iotg_imx8plus_addon_i2c_bus[IOTG_IMX8PLUS_ADDON_LAST] = {
 /* I2C device addresses array */
 static uint iotg_imx8plus_addon_i2c_addr[IOTG_IMX8PLUS_ADDON_LAST] = {
 	[IOTG_IMX8PLUS_ADDON_M2EMMC] = 0x20,
+	[IOTG_IMX8PLUS_ADDON_M2M2] = 0x21,
 	[IOTG_IMX8PLUS_ADDON_M2ADC] = 0x48,
 	[IOTG_IMX8PLUS_ADDON_M2TPM] = 0x54,
 };
