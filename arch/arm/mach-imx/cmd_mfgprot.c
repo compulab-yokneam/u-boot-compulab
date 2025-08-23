72207e60e270f3bdf34ab22f70d9e9934f551924
Valentin Raevsky <valentin@compulab.co.il>
2025-08-23 16:16:42 +0300

    fsl: Allow mfgprot work on 2021.04
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- arch/arm/mach-imx/cmd_mfgprot.c
+++ arch/arm/mach-imx/cmd_mfgprot.c
@@ -1,8 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0+
 /*
  * Copyright 2016 Freescale Semiconductor, Inc.
  * Copyright 2017 NXP
- *
- * SPDX-License-Identifier: GPL-2.0+
  *
  * These commands enable the use of the CAAM MPPubK-generation and MPSign
  * functions in supported i.MX devices.
@@ -10,7 +9,7 @@
 
 #include <command.h>
 #include <common.h>
-#include <environment.h>
+#include <env.h>
 #include <mapmem.h>
 #include <memalign.h>
 #ifdef CONFIG_IMX_CAAM_MFG_PROT
@@ -21,8 +20,6 @@
 #include <asm/io.h>
 #include <asm/arch/sci/sci.h>
 #endif
-
-DECLARE_GLOBAL_DATA_PTR;
 
 /**
  * do_mfgprot() - Handle the "mfgprot" command-line command
@@ -78,8 +75,8 @@ static int do_mfgprot(struct cmd_tbl *cmdtp, int flag, int argc, char *const arg
 		if (argc != 4)
 			return CMD_RET_USAGE;
 
-		m_addr = simple_strtoul(argv[2], NULL, 16);
-		m_size = simple_strtoul(argv[3], NULL, 10);
+		m_addr = hextoul(argv[2], NULL);
+		m_size = dectoul(argv[3], NULL);
 		m_ptr = map_physmem(m_addr, m_size, MAP_NOCACHE);
 		if (!m_ptr)
 			return -ENOMEM;
