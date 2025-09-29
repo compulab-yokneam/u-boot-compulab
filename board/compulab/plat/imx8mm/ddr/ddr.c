1504ead46c9e76eb2431183dc0d45c5dc41af5e8
Kirill Kapranov <kirill.kapranov@compulab.co.il>
2025-09-29 22:09:35 +0300

    imx8mm:ddr: Introduce Etron 4G chip 0xff070018
    Signed-off-by: Kirill Kapranov <kirill.kapranov@compulab.co.il>

--- board/compulab/plat/imx8mm/ddr/ddr.c
+++ board/compulab/plat/imx8mm/ddr/ddr.c
@@ -72,7 +72,7 @@ struct lpddr4_desc {
 
 #define DEFAULT (('D' << 24) + ('E' << 16 ) + ( 'F' << 8 ) + 'A')
 static const struct lpddr4_desc lpddr4_array[] = {
-	{ .name = "Etron",	.id = 0xff070010, .subind = 0xff, .size = 2048, .count = 1, .timing = &ucm_dram_timing_ff070010},
+	{ .name = "Etron",	.id = 0xff070018, .subind = 0xff, .size = 4096, .count = 1, .timing = &dram_timing_ff070018},
 #if 0
 	{ .name = "Etron",	.id = 0x1a000008, .subind = 0xff, .size = 1024, .count = 1, .timing = &ucm_dram_timing_1a000008},
 	{ .name = "ISSI",	.id = 0x13000210, .subind = 0xff, .size = 4096, .count = 1, .timing = &ucm_dram_timing_13000210}, // TBD -- fake assignement!
