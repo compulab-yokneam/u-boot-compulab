5a952ae85d5d6b9977accd1f2f5fc55993232a74
Bernhard Wörndl-Aichriedler <bwa@xdevelop.at>
2026-07-08 09:52:56 +0200

    Revert "imx8m-mini:spl:ddr: Integrate timing blocks to SPL"
    This reverts commit 7f36d59d8dfd333eb2a578dade6c062db7dc9da8.

--- board/compulab/plat/imx8mm/ddr/ddr.c
+++ board/compulab/plat/imx8mm/ddr/ddr.c
@@ -27,13 +27,8 @@ u8 cl_eeprom_set_subind(u8 subind);
 
 /* Placeholder to be filled with a real timing table read from MMC */
 __attribute__ ((section (".data")))
-static struct lpddr4_timing_block timing_block[] = {
-	{.magic = LPDDR_EMPTY_MAGIC,},
-	{.magic = LPDDR_EMPTY_MAGIC,},
-	{.magic = LPDDR_EMPTY_MAGIC,},
-	{.magic = LPDDR_EMPTY_MAGIC,},
-	{.magic = LPDDR_EMPTY_MAGIC,},
-	{.magic = LPDDR_EMPTY_MAGIC,},
+static struct lpddr4_timing_block timing_block = {
+	.magic = LPDDR_EMPTY_MAGIC,
 };
 
 int mmc_init(struct mmc *mmc);
@@ -70,32 +65,32 @@ static unsigned int lpddr4_mr_read_and_refine(unsigned int mr_rank, unsigned int
 }
 #define DEFAULT (('D' << 24) + ('E' << 16 ) + ( 'F' << 8 ) + 'A')
 static const struct timing_desc lpddr4_array[] = {
-	{ .name = "Kingston",	.id = 0xff070010, .subind = 0x04, .timing_sign = 0xff070110},//C3222PM4CDGUI-U
-	{ .name = "Kingston",	.id = 0xff070010, .subind = 0x02, .timing_sign = 0xff070010},//D1621PM4CDGUI
+	{ .name = "Kingston",	.id = 0xff070010, .subind = 0x04, .size = 4096, .timing_sign = 0xff070110},//C3222PM4CDGUI-U
+	{ .name = "Kingston",	.id = 0xff070010, .subind = 0x02, .size = 2048, .timing_sign = 0xff070010},//D1621PM4CDGUI
 #ifdef CONFIG_TARGET_MCM_IMX8M_MINI
-	{ .name = "Nanya",	.id = 0x05000010, .subind = 0xff, .timing_sign = 0x01061010},
+	{ .name = "Nanya",	.id = 0x05000010, .subind = 0xff, .size = 2048, .timing_sign = 0x01061010},
 #else
-	{ .name = "Nanya",	.id = 0x05000010, .subind = 0xff, .timing_sign = 0x05000010},
+	{ .name = "Nanya",	.id = 0x05000010, .subind = 0xff, .size = 2048, .timing_sign = 0x05000010},
 #endif
-	{ .name = "ISSI",	.id = 0x1b000008, .subind = 0xff, .timing_sign = 0x1b000008}, //IS43LQ32256B-062BLI
-	{ .name = "ISSI",	.id = 0x1b010008, .subind = 0xff, .timing_sign = 0x1b000008}, //IS43LQ32256C-046BLI
-	{ .name = "Etron",	.id = 0x1a000008, .subind = 0xff, .timing_sign = 0x1b000008}, //EM6LF32MBAJB-46ISH
-	{ .name = "Winbond",	.id = 0x08000008, .subind = 0xff, .timing_sign = 0x1b000008}, //W66DP2RQQAGJ
-	//{ .name = "ISSI",	.id = 0x13000210, .subind = 0x04, .timing_sign = 0}, //
-	{ .name = "ISSI",	.id = 0x13000210, .subind = 0x02, .timing_sign = 0xff070010}, //IS43LQ32512A-053BLI
-	{ .name = "Samsung",	.id = 0x01061010, .subind = 0x04, .timing_sign = 0xff000110},
-	{ .name = "Samsung",	.id = 0x01061010, .subind = 0x02, .timing_sign = 0x01061010},
-	{ .name = "Samsung",	.id = 0x01080010, .subind = 0x04, .timing_sign = 0xff000110},
-	{ .name = "Samsung",	.id = 0x01080010, .subind = 0x02, .timing_sign = 0x01061010},
-	{ .name = "Samsung",	.id = 0x01050008, .subind = 0xff, .timing_sign = 0x01050008},
-	{ .name = "Samsung",	.id = 0x01060008, .subind = 0xff, .timing_sign = 0x01050008},
-	{ .name = "Alliance",	.id = 0x52000008, .subind = 0xff, .timing_sign = 0x01050008},
-	{ .name = "Kingston",	.id = 0xff050010, .subind = 0xff, .timing_sign = 0x01061010},
-	{ .name = "Kingston",	.id = 0xff000010, .subind = 0x04, .timing_sign = 0xff000110},
-	{ .name = "Kingston",	.id = 0xff000010, .subind = 0x02, .timing_sign = 0x01061010},
-	{ .name = "Micron",	.id = 0xff020008, .subind = 0xff, .timing_sign = 0xff020008},
-	{ .name = "Micron",	.id = 0xff000110, .subind = 0xff, .timing_sign = 0xff000110},
-	{ .name = "Etron",	.id = 0xff070018, .subind = 0xff, .timing_sign = 0xff070018}, //EM6LH32MVAJA
+	{ .name = "ISSI",	.id = 0x1b000008, .subind = 0xff, .size = 1024, .timing_sign = 0x1b000008}, //IS43LQ32256B-062BLI
+	{ .name = "ISSI",	.id = 0x1b010008, .subind = 0xff, .size = 1024, .timing_sign = 0x1b000008}, //IS43LQ32256C-046BLI
+	{ .name = "Etron",	.id = 0x1a000008, .subind = 0xff, .size = 1024, .timing_sign = 0x1b000008}, //EM6LF32MBAJB-46ISH
+	{ .name = "Winbond",	.id = 0x08000008, .subind = 0xff, .size = 1024, .timing_sign = 0x1b000008}, //W66DP2RQQAGJ
+	//{ .name = "ISSI",	.id = 0x13000210, .subind = 0x04, .size = 4096, .timing_sign = 0}, //
+	{ .name = "ISSI",	.id = 0x13000210, .subind = 0x02, .size = 2048, .timing_sign = 0xff070010}, //IS43LQ32512A-053BLI
+	{ .name = "Samsung",	.id = 0x01061010, .subind = 0x04, .size = 4096, .timing_sign = 0xff000110},
+	{ .name = "Samsung",	.id = 0x01061010, .subind = 0x02, .size = 2048, .timing_sign = 0x01061010},
+	{ .name = "Samsung",	.id = 0x01080010, .subind = 0x04, .size = 4096, .timing_sign = 0xff000110},
+	{ .name = "Samsung",	.id = 0x01080010, .subind = 0x02, .size = 2048, .timing_sign = 0x01061010},
+	{ .name = "Samsung",	.id = 0x01050008, .subind = 0xff, .size = 1024, .timing_sign = 0x01050008},
+	{ .name = "Samsung",	.id = 0x01060008, .subind = 0xff, .size = 1024, .timing_sign = 0x01050008},
+	{ .name = "Alliance",	.id = 0x52000008, .subind = 0xff, .size = 1024, .timing_sign = 0x01050008},
+	{ .name = "Kingston",	.id = 0xff050010, .subind = 0xff, .size = 2048, .timing_sign = 0x01061010},
+	{ .name = "Kingston",	.id = 0xff000010, .subind = 0x04, .size = 4096, .timing_sign = 0xff000110},
+	{ .name = "Kingston",	.id = 0xff000010, .subind = 0x02, .size = 2048, .timing_sign = 0x01061010},
+	{ .name = "Micron",	.id = 0xff020008, .subind = 0xff, .size = 2048, .timing_sign = 0xff020008},
+	{ .name = "Micron",	.id = 0xff000110, .subind = 0xff, .size = 4096, .timing_sign = 0xff000110},
+	{ .name = "Etron",	.id = 0xff070018, .subind = 0xff, .size = 4096, .timing_sign = 0xff070018}, //EM6LH32MVAJA
 };
 
 static unsigned int lpddr4_get_mr(void)
@@ -118,19 +113,19 @@ static unsigned int lpddr4_get_mr(void)
 }
 
 static void spl_tcm_init(struct lpddr4_tcm_desc *lpddr4_tcm_desc) {
-	if (lpddr4_tcm_desc->sign == DEFAULT)
-		return;
+    if (lpddr4_tcm_desc->sign == DEFAULT)
+	    return;
 
-	lpddr4_tcm_desc->sign = DEFAULT;
-	lpddr4_tcm_desc->index = 0;
+    lpddr4_tcm_desc->sign = DEFAULT;
+    lpddr4_tcm_desc->index = 0;
 }
 
 static void spl_tcm_fini(struct lpddr4_tcm_desc *lpddr4_tcm_desc) {
-	if (lpddr4_tcm_desc->sign != DEFAULT)
-		return;
+    if (lpddr4_tcm_desc->sign != DEFAULT)
+	    return;
 
-	lpddr4_tcm_desc->sign = ~DEFAULT;
-	lpddr4_tcm_desc->index = 0;
+    lpddr4_tcm_desc->sign = ~DEFAULT;
+    lpddr4_tcm_desc->index = 0;
 }
 
 static int spl_mmc_get_device_index(u32 boot_device)
@@ -174,7 +169,7 @@ static int spl_mmc_find_device(struct mmc **mmcp, u32 boot_device)
 	if (err) {
 #ifdef CONFIG_SPL_LIBCOMMON_SUPPORT
 		printf("spl: could not find mmc device %d. error: %d\n",
-			mmc_dev, err);
+		       mmc_dev, err);
 #endif
 		return err;
 	}
@@ -200,9 +195,9 @@ static int read_timing_from_mmc(int idx)
 
 	bootdev = spl_boot_device();
 
-	image.load_addr = (long unsigned int)&timing_block[0],
+	image.load_addr = (long unsigned int)&timing_block,
 	image.boot_device = bootdev,
-	image.size = sizeof(timing_block[0]),
+	image.size = sizeof(timing_block),
 
 	err = spl_mmc_find_device(&mmc, bootdev);
 	if(err) {
@@ -220,23 +215,23 @@ static int read_timing_from_mmc(int idx)
 
 	if (mmc->part_support && mmc->part_config != MMCPART_NOAVAILABLE) {
 		hwpart = EXT_CSD_EXTRACT_BOOT_PART(mmc->part_config);
-		hwpart = (7 == hwpart)? 0 : hwpart; // the User part is denoted as 7, but 0 is to switch to
+		hwpart = (7 == hwpart)? 0 : hwpart; // Dont ask me, why User part is denoted as 7, but 0 is to switch to
 		mmc_switch_part(mmc, hwpart);
 	}
 	bd = mmc_get_blk_desc(mmc);
-	sec_cnt = (sizeof(timing_block[0]) + bd->blksz - 1) / bd->blksz;
+	sec_cnt = (sizeof(timing_block) + bd->blksz - 1) / bd->blksz;
 
 	count = blk_dread(bd,
 		LPDDR4_TIMINGS_BIN_SECTOR + IMX_BOOT_SEEK * 2 + idx * sec_cnt,
 		sec_cnt,
-		&timing_block[0]);
+		&timing_block);
 	if(sec_cnt != count) {
 		printf("%s: %d sector read %d sector necessary\n",
 				__func__, count, sec_cnt);
 		while(42);
 	}
 
-	if(!!strncmp(timing_block[0].magic, LPDDR_BLOCK_MAGIC, sizeof(LPDDR_BLOCK_MAGIC))) {
+	if(!!strncmp(timing_block.magic, LPDDR_BLOCK_MAGIC, sizeof(LPDDR_BLOCK_MAGIC))) {
 		printf("No LPDDR block magic found\n");
 		return 1;
 	}
@@ -244,63 +239,46 @@ static int read_timing_from_mmc(int idx)
 	return 0;
 }
 
-/* Update pointers to get a timing structure operable, basing on a block, read from a storage or found in SPL*/
-static void relocate_timing_block(int num)
+/* Update pointers to get an operable timing structure, basing on the block, read from a storage*/
+static void relocate_timing_block(void)
 {
-	timing_block[num].ddr_dram_fsp_msg[0].fsp_cfg = timing_block[num].ddr_fsp0_cfg;
-	timing_block[num].ddr_dram_fsp_msg[1].fsp_cfg = timing_block[num].ddr_fsp1_cfg;
-	timing_block[num].ddr_dram_fsp_msg[2].fsp_cfg = timing_block[num].ddr_fsp2_cfg;
-	timing_block[num].ddr_dram_fsp_msg[3].fsp_cfg = timing_block[num].ddr_fsp0_2d_cfg;
-	timing_block[num].dram_timing.fsp_msg = timing_block[num].ddr_dram_fsp_msg;
+	timing_block.ddr_dram_fsp_msg[0].fsp_cfg = timing_block.ddr_fsp0_cfg;
+	timing_block.ddr_dram_fsp_msg[1].fsp_cfg = timing_block.ddr_fsp1_cfg;
+	timing_block.ddr_dram_fsp_msg[2].fsp_cfg = timing_block.ddr_fsp2_cfg;
+	timing_block.ddr_dram_fsp_msg[3].fsp_cfg = timing_block.ddr_fsp0_2d_cfg;
+	timing_block.dram_timing.fsp_msg = timing_block.ddr_dram_fsp_msg;
 
-	timing_block[num].dram_timing.ddrc_cfg = timing_block[num].ddr_ddrc_cfg;
-	timing_block[num].dram_timing.ddrphy_cfg = timing_block[num].ddr_ddrphy_cfg;
-	timing_block[num].dram_timing.ddrphy_pie = timing_block[num].ddr_phy_pie;
-
-	// Initialize the common part of all trainigs
-	timing_block[num].dram_timing.ddrphy_trained_csr = ddr_ddrphy_trained_csr;
-	timing_block[num].dram_timing.ddrphy_trained_csr_num = ARRAY_SIZE(ddr_ddrphy_trained_csr);
+	timing_block.dram_timing.ddrc_cfg = timing_block.ddr_ddrc_cfg;
+	timing_block.dram_timing.ddrphy_cfg = timing_block.ddr_ddrphy_cfg;
+	timing_block.dram_timing.ddrphy_trained_csr = timing_block.ddr_ddrphy_trained_csr;
+	timing_block.dram_timing.ddrphy_pie = timing_block.ddr_phy_pie;
 }
 
-/* Find required timing block in SPL body; if no one presented find on storage
-* return index in array of timing blocks
-* block, found on a storage is copied @ index 0
-*/
 static int find_timing_block(unsigned long id)
 {
-unsigned i;
 	// First, check, is there a single timing block, incorporated in SPL code directly.
-	for(i=0; ARRAY_SIZE(timing_block) > i; ++i) {
-		if(!strncmp(timing_block[i].magic, LPDDR_SINGLE_MAGIC, sizeof(LPDDR_SINGLE_MAGIC))) {
-			printf("Timing block[%i] 0x%x integrated ", i, timing_block[i].id);
-			if(id == timing_block[i].id) {
-				printf("match\n");
-				relocate_timing_block(i);
-				return i;
-			} else { //The boot loader with an incorporated timing has no external timings available
-				printf("unmatch (0x%x required)\n", id);
-			}
-		} else {
-			break;
+	if(!strncmp(timing_block.magic, LPDDR_SINGLE_MAGIC, sizeof(LPDDR_SINGLE_MAGIC))) {
+		printf("Single timing block 0x%x incorporated ", timing_block.id);
+		if(id == timing_block.id) {
+			printf("match\n", timing_block.id);
+			relocate_timing_block();
+			return 0;
+		} else { //The boot loader with an incorporated timing has no external timings available
+			printf("unmatch (0x%x required)\n", id);
+			return 1;
 		}
 	}
-	if(0 != i) { // This is an SPL with integrated timings, however no one match
-		printf("No one of integrated timing matches (0x%x required)\n", id);
-		mdelay(60); // Let printf complete
-		do_reset(NULL,0,0,NULL);
-	}
-
-	// This is an SPL w/o integrated timing; search a storage
-	for(i=0; ARRAY_SIZE(lpddr4_array) > i; ++i) { // Very rough upper limit, just in case
+	// No incorporated timing present -- search on a storage
+	for(int i=0; ARRAY_SIZE(lpddr4_array) > i; ++i) { // Very rough upper limit, just in case
 		printf("LPDDR timing 0x%x search entry %i\t", id, i);
 		int ret = read_timing_from_mmc(i);
 		if(0 == ret) {
-			if(id == timing_block[0].id) {
-				printf("0x%x found\n", timing_block[0].id);
-				relocate_timing_block(0);
+			if(id == timing_block.id) {
+				printf("0x%x found\n", timing_block.id);
+				relocate_timing_block();
 				return 0;
 			} else {
-				printf("not found (0x%x)\n", timing_block[0].id);
+				printf("not found (0x%x)\n", timing_block.id);
 			}
 		}
 		else if( 0 > ret) {
@@ -308,7 +286,7 @@ unsigned i;
 			return ret;
 		}
 		else if( 0 < ret) {
-			printf("end\n");
+			printf("not found\n", id);
 			return -ENOENT;
 		}
 	}
@@ -326,7 +304,6 @@ void spl_dram_init(void)
 	unsigned int ddr_info_mrr = 0xdeadbeef;
 	unsigned int ddr_found = 0;
 	int i = 0;
-	int ind = 0;
 
 	struct lpddr4_tcm_desc *lpddr4_tcm_desc = (struct lpddr4_tcm_desc *) SPL_TCM_DATA;
 
@@ -363,17 +340,20 @@ void spl_dram_init(void)
 	}
 	ddr_info = lpddr4_array[i].id;
 
-	ind = find_timing_block(lpddr4_array[i].timing_sign);
-	if(0 > ind) {
+	if(0 != find_timing_block(lpddr4_array[i].timing_sign)) {
 		printf("DDRINFO: Timing block ID = 0x%x[%i] for 0x%x.0x%x not found. A non-supported configuration.\n",
 			lpddr4_array[i].timing_sign, i, lpddr4_array[i].id , lpddr4_array[i].subind);
 		while ( 1 ) {};
 	}
 
 	printf("DDRINFO(%s): %s %dG @ %d MHz\n", (ddr_found ? "eeprom" : "try" ), lpddr4_array[i].name,
-		timing_block[ind].size, timing_block[ind].dram_timing.fsp_table[0]);
+		lpddr4_array[i].size, timing_block.dram_timing.fsp_table[0]);
 
-	if (ddr_init(&timing_block[ind].dram_timing)) {
+	//Initialize the common part of all trainigs
+	//lpddr4_array[i].timing->ddrphy_trained_csr = ddr_ddrphy_trained_csr;
+	//lpddr4_array[i].timing->ddrphy_trained_csr_num = ARRAY_SIZE(ddr_ddrphy_trained_csr);
+
+	if (ddr_init(&timing_block.dram_timing)) {
 		SPL_TCM_INIT;
 		do_reset(NULL,0,0,NULL);
 	}
@@ -418,12 +398,13 @@ void spl_dram_init(void)
 	  /* For debug purpouse only. To override the real memsize */
 		unsigned int ddr_tcm_size = cl_eeprom_get_osize();
 		if ((ddr_tcm_size == 0) || (ddr_tcm_size == -1))
-			ddr_tcm_size = timing_block[ind].size;
+			ddr_tcm_size = timing_block.size;
 
 		lpddr4_tcm_desc->size = ddr_tcm_size;
 	}
 #else
-	lpddr4_tcm_desc->size = timing_block[ind].size;
+	lpddr4_tcm_desc->size = lpddr4_array[i].size;
+	lpddr4_tcm_desc->sign = lpddr4_array[i].id;
 #endif
 	lpddr4_tcm_desc->sign = lpddr4_array[i].id;
 	lpddr4_tcm_desc->timing = lpddr4_array[i].timing_sign;
--- board/compulab/plat/imx8mm/ddr/lpddr4_timing_block.h
+++ board/compulab/plat/imx8mm/ddr/lpddr4_timing_block.h
@@ -17,13 +17,14 @@ struct timing_desc {
 	 * Default value 0xff
 	 */
 	u8	subind;
-	u32	timing_sign; // A uniq signature of timing block to be loaded for this chip
+	u16	size; // Total size, MB, to be displayed
+	u64	timing_sign; // A uniq signature of timing block to be loaded for this chip
 };
 
 // Array sizes are assigned bigger than required. An actual length is assigned in each timing file individually
 #define LPDDR_EMPTY_MAGIC "LPDDREMPTYMAGIC"
-#define LPDDR_BLOCK_MAGIC "LPDDRBLOCKMAGIC"	// All magics are the same size
-#define LPDDR_SINGLE_MAGIC "LPDDRSINGLMAGIC"
+#define LPDDR_SINGLE_MAGIC "LPDDRONEMAGIC"
+#define LPDDR_BLOCK_MAGIC "LPDDRBLOCKMAGIC"
 #define _DDR_DDRC_CFG_NUM 110
 #define _DDR_DDRPHY_CFG_NUM 210
 #define _DDR_DDRPHY_TRAINED_CSR_NUM 730
@@ -33,12 +34,11 @@ struct timing_desc {
 #define DDR_DRAM_FSP_MSG_NUM 4
 struct lpddr4_timing_block {
 	char magic[sizeof(LPDDR_BLOCK_MAGIC)];
-	int size;
 	u32 id;
 	// Training payload
 	struct dram_cfg_param ddr_ddrc_cfg[_DDR_DDRC_CFG_NUM];
 	struct dram_cfg_param ddr_ddrphy_cfg[_DDR_DDRPHY_CFG_NUM];
-	struct dram_cfg_param* ddr_ddrphy_trained_csr;
+	struct dram_cfg_param ddr_ddrphy_trained_csr[_DDR_DDRPHY_TRAINED_CSR_NUM];
 	struct dram_cfg_param ddr_fsp0_cfg[_DDR_FSP_CFG_NUM];
 	struct dram_cfg_param ddr_fsp1_cfg[_DDR_FSP_CFG_NUM];
 	struct dram_cfg_param ddr_fsp2_cfg[_DDR_FSP_CFG_NUM];
