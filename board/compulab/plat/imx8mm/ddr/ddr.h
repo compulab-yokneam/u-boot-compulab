cd7a652fd32a34bb3ac0793ecb36d674eae72ad8
Bernhard Wörndl-Aichriedler <bwa@xdevelop.at>
2026-07-08 09:53:09 +0200

    Revert "imx8mm:spl:ddr: Move DDR timing blocks from spl to storage"
    This reverts commit f06ece013f22a3bf673d95ecd08ca711359cfc8a.

--- board/compulab/plat/imx8mm/ddr/ddr.h
+++ board/compulab/plat/imx8mm/ddr/ddr.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Compulab Ltd.
+ * Copyright 2017 NXP
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
@@ -7,15 +7,22 @@
 #ifndef __DDR_H__
 #define __DDR_H__
 
+extern struct dram_timing_info ucm_dram_timing_ff020008;
+extern struct dram_timing_info ucm_dram_timing_ff000110;
+extern struct dram_timing_info ucm_dram_timing_01061010;
+extern struct dram_timing_info ucm_dram_timing_01050008;
+extern struct dram_timing_info ucm_dram_timing_05000010;
+extern struct dram_timing_info ucm_dram_timing_1b000008;
+
 void spl_dram_init(void);
 
 #define TCM_DATA_CFG 0x7e0000
 
 struct lpddr4_tcm_desc {
 	unsigned int size;
-	unsigned int sign; // DDR quirq, read from MRR
+	unsigned int sign;
 	unsigned int index;
-	unsigned int timing; // A sign of timing block, the DDR was trained with
+	unsigned int count;
 };
 
 #endif
